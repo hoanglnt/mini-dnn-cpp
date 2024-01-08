e0500c8e420b21250788a9d2ff6ebc0e909366b2
hoanglnt1302 <hoanglnt1302@gmail.com>
2024-01-08 10:02:22 +0700

    init

---
+++ build/src/CMakeFiles/MiniDNNLib.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/d/Users/OneDrive - VNU-HCMUS/Documents/Study/4Y1S/03.Parallel_programming/p/mini-dnn-cpp/src/layer/ave_pooling.cc" "src/CMakeFiles/MiniDNNLib.dir/layer/ave_pooling.cc.o" "gcc" "src/CMakeFiles/MiniDNNLib.dir/layer/ave_pooling.cc.o.d"
+  "/mnt/d/Users/OneDrive - VNU-HCMUS/Documents/Study/4Y1S/03.Parallel_programming/p/mini-dnn-cpp/src/layer/conv.cc" "src/CMakeFiles/MiniDNNLib.dir/layer/conv.cc.o" "gcc" "src/CMakeFiles/MiniDNNLib.dir/layer/conv.cc.o.d"
+  "/mnt/d/Users/OneDrive - VNU-HCMUS/Documents/Study/4Y1S/03.Parallel_programming/p/mini-dnn-cpp/src/layer/fully_connected.cc" "src/CMakeFiles/MiniDNNLib.dir/layer/fully_connected.cc.o" "gcc" "src/CMakeFiles/MiniDNNLib.dir/layer/fully_connected.cc.o.d"
+  "/mnt/d/Users/OneDrive - VNU-HCMUS/Documents/Study/4Y1S/03.Parallel_programming/p/mini-dnn-cpp/src/layer/max_pooling.cc" "src/CMakeFiles/MiniDNNLib.dir/layer/max_pooling.cc.o" "gcc" "src/CMakeFiles/MiniDNNLib.dir/layer/max_pooling.cc.o.d"
+  "/mnt/d/Users/OneDrive - VNU-HCMUS/Documents/Study/4Y1S/03.Parallel_programming/p/mini-dnn-cpp/src/layer/relu.cc" "src/CMakeFiles/MiniDNNLib.dir/layer/relu.cc.o" "gcc" "src/CMakeFiles/MiniDNNLib.dir/layer/relu.cc.o.d"
+  "/mnt/d/Users/OneDrive - VNU-HCMUS/Documents/Study/4Y1S/03.Parallel_programming/p/mini-dnn-cpp/src/layer/sigmoid.cc" "src/CMakeFiles/MiniDNNLib.dir/layer/sigmoid.cc.o" "gcc" "src/CMakeFiles/MiniDNNLib.dir/layer/sigmoid.cc.o.d"
+  "/mnt/d/Users/OneDrive - VNU-HCMUS/Documents/Study/4Y1S/03.Parallel_programming/p/mini-dnn-cpp/src/layer/softmax.cc" "src/CMakeFiles/MiniDNNLib.dir/layer/softmax.cc.o" "gcc" "src/CMakeFiles/MiniDNNLib.dir/layer/softmax.cc.o.d"
+  "/mnt/d/Users/OneDrive - VNU-HCMUS/Documents/Study/4Y1S/03.Parallel_programming/p/mini-dnn-cpp/src/loss/cross_entropy_loss.cc" "src/CMakeFiles/MiniDNNLib.dir/loss/cross_entropy_loss.cc.o" "gcc" "src/CMakeFiles/MiniDNNLib.dir/loss/cross_entropy_loss.cc.o.d"
+  "/mnt/d/Users/OneDrive - VNU-HCMUS/Documents/Study/4Y1S/03.Parallel_programming/p/mini-dnn-cpp/src/loss/mse_loss.cc" "src/CMakeFiles/MiniDNNLib.dir/loss/mse_loss.cc.o" "gcc" "src/CMakeFiles/MiniDNNLib.dir/loss/mse_loss.cc.o.d"
+  "/mnt/d/Users/OneDrive - VNU-HCMUS/Documents/Study/4Y1S/03.Parallel_programming/p/mini-dnn-cpp/src/mnist.cc" "src/CMakeFiles/MiniDNNLib.dir/mnist.cc.o" "gcc" "src/CMakeFiles/MiniDNNLib.dir/mnist.cc.o.d"
+  "/mnt/d/Users/OneDrive - VNU-HCMUS/Documents/Study/4Y1S/03.Parallel_programming/p/mini-dnn-cpp/src/network.cc" "src/CMakeFiles/MiniDNNLib.dir/network.cc.o" "gcc" "src/CMakeFiles/MiniDNNLib.dir/network.cc.o.d"
+  "/mnt/d/Users/OneDrive - VNU-HCMUS/Documents/Study/4Y1S/03.Parallel_programming/p/mini-dnn-cpp/src/optimizer/sgd.cc" "src/CMakeFiles/MiniDNNLib.dir/optimizer/sgd.cc.o" "gcc" "src/CMakeFiles/MiniDNNLib.dir/optimizer/sgd.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
